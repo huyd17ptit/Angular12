57c3d564d73ec9dcfb3b1095fb0e0f7ea4dbce96
huyd17ptit <huybohotdps4@gmail.com>
2022-03-06 14:33:27 +0700

    ngFor

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,12 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   public name = 'Quang Huy';
-
   public age = 25;
+  public traiCay = ['Táo','Nho','Cam','Xoài'];
+  public traiCay2 = [
+    {ten: 'Táo', gia: 12, haGia: false},
+    {ten: 'Nho', gia: 15, haGia: true},
+    {ten: 'Cam', gia: 10, haGia: false},
+    {ten: 'Xoài', gia: 5, haGia: true}
+  ];
 
   constructor() { }
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
+    console.log('trai cay = ', this.traiCay);
   }
-
 }
