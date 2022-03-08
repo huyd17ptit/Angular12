aaf7f07676377127963b9869960071c63413bc4b
huyd17ptit <huybohotdps4@gmail.com>
2022-03-08 13:27:21 +0700

    Forms

---
+++ src/app/template-driven-form/template-driven-form.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-template-driven-form',
+  templateUrl: './template-driven-form.component.html',
+  styleUrls: ['./template-driven-form.component.scss']
+})
+export class TemplateDrivenFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onClickSubmit(result: any) {
+    console.log("You have entered : " + result.username); 
+  }
+
+}
